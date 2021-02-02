6b5a5bd7045da2e6e4e9184ae64301b17d26255f
Jacob Makil <makilj@sheridancollege.ca>
2021-02-02 10:53:08 -0500

    First Commit

---
+++ src/student/list/StudentList.java
@@ -0,0 +1,25 @@
+package student.list;
+import java.util.Scanner;
+
+/**
+ *
+ * @author makil
+ */
+public class StudentList {
+
+ 
+    public static void main(String[] args) {
+     String[] students = new String [5];
+     Scanner a = new Scanner(System.in);
+     
+     System.out.println("Please Enter the name of the 5 students in the class: ");
+     for (int i=0;i<students.length;i++ ){
+      students[i] = a.nextLine();
+     }
+     System.out.println("The name of the students are: ");
+     for (String name: students){
+         System.out.println(name);
+     }
+    }
+    
+}
